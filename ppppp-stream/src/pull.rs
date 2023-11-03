dcc41909f94c766d4d870d3a55ac0267a33f0de8
Michael Williams <michael.williams@enspiral.com>
2023-11-03 22:14:54 +1300

    wait... did this work?

--- ppppp-stream/src/pull.rs
+++ ppppp-stream/src/pull.rs
@@ -1,5 +1,8 @@
+use std::fmt::Debug;
+
 use futures::{pin_mut, StreamExt};
 use napi::bindgen_prelude::{spawn, Error};
+use pin_utils::unsafe_pinned;
 
 use crate::Source;
 
@@ -29,8 +32,55 @@ pub enum EndState {
     Done,
 }
 pub type End = Option<EndState>;
-type PullSource<Value> = Box<dyn Fn(End, Box<dyn Fn(End, Option<Value>) + Send>)>;
+// type PullSource<Value> = Box<dyn Fn(End, Box<dyn Fn(End, Option<Value>) + Send>)>;
 
+pub struct PullSource<Src> {
+    source: Src,
+}
+
+impl<Src: Unpin> Unpin for PullSource<Src> {}
+
+impl<Src> PullSource<Src>
+where
+    Src: Source,
+{
+    unsafe_pinned!(source: Src);
+
+    pub fn new(source: Src) -> Self {
+        Self { source }
+    }
+}
+
+impl<Src, Value> PullSource<Src>
+where
+    Src: Source<Item = Result<Value, Error>> + Send + 'static,
+    Value: Debug,
+{
+    fn read<Cb>(self, end: End, cb: Cb)
+    where
+        Cb: Fn(End, Option<Value>) + Unpin + Send + 'static,
+    {
+        let mut source = Box::pin(self.source);
+
+        if let Some(end_state) = end {
+            match end_state {
+                EndState::Error(err) => cb(Some(EndState::Error(err)), None),
+                EndState::Done => cb(Some(EndState::Done), None),
+            };
+            return;
+        }
+
+        spawn(async move {
+            match source.next().await {
+                Some(Ok(value)) => cb(None, Some(value)),
+                Some(Err(err)) => cb(Some(EndState::Error(err)), None),
+                None => cb(Some(EndState::Done), None),
+            }
+        });
+    }
+}
+
+/*
 fn to_pull_source<
     Value,
     Src: Source<Item = Result<Value, Error>> + Unpin + Send + Sync + 'static,
@@ -57,3 +107,4 @@ fn to_pull_source<
         });
     })
 }
+*/
