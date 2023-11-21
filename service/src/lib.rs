5103d5c395e50f8e7e5a96df30ec235fa4016778
Michael Williams <michael.williams@enspiral.com>
2023-11-21 22:30:11 +1300

    musings

--- service/src/lib.rs
+++ service/src/lib.rs
@@ -21,6 +21,33 @@
 //   if one service will call another service, they probably will be separate anyways.
 //   one service can get a client connection to another service.
 //   rather than in SSB where everything is by default able to touch anything else.
+//
+// looking at quic-rpc, i notice:
+//   - the server handler routing is done manually, not automatically. so that'd help.
+//   - and same with the client calls.
+// so maybe we can do something similar:
+//
+// match msg {
+//   Ping(args) => {
+//      server.handle_async(Ping, args)
+//   }
+// }
+//
+// the main challenge here is that we need a single type of represent incoming messages.
+// but muxrpc has a weird (for Rust) structure:
+//
+// {
+//  "name": ["createHistoryStream"],
+//  "type": "source",
+//  "args": [{"id": "@FCX/tsDLpubCPKKfIrw4gc+SQkHcaD17s7GI6i/ziWY=.ed25519"}]
+// }
+//
+// maybe we just do the JSON + MuxRPC part separately.
+//   like a name + type -> deserialized args struct registry.
+//   where we choose which methods to expose over MuxRPC.
+//   then we can convert these args structs into the top-level enum.
+//
+// 
 
 use std::{convert::Infallible, future::Future};
 
